2c7b5282d019ead78db977370af8052be1c89d42
Khoa Nguyen Dang <khoanguyend7@nashtechglobal.com>
2023-10-03 15:08:00 +0700

    First commit

---
+++ provider/NextUI.js
@@ -0,0 +1,12 @@
+"use client"
+import * as React from "react";
+
+import {NextUIProvider} from "@nextui-org/react";
+
+export default function NextUI({ children }) {
+  return (
+    <NextUIProvider>
+      { children }
+    </NextUIProvider>
+  );
+}
